e9dc46a6b4444e9e8aec4d918caa268c6d2d454b
Jeff Potts <jeff@2020creative.com>
2014-10-14 12:04:08 -0500

    wip

--- ad-spot-b.php
+++ ad-spot-b.php
@@ -4,7 +4,7 @@
                     
                     
     <?php 
-        $post_id = '4043'; //2917 and 4043
+        $post_id = '4043'; //2917 is DEV, 4043 is live
         $featured_img = get_post_thumbnail_id( $post_id );
         $ad_2_img = wp_get_attachment_url( $featured_img );
         $ad_2_link = get_post_meta( $post_id, 'ad_spot_2_link', true ); 
